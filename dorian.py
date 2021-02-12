12744654f6b14aac36ef0e2d37864f7bd6d6908f
kenodressel <hire@keno.digital>
2021-02-12 19:57:12 +0100

    feat: comments & handle numbers only case

--- dorian.py
+++ dorian.py
@@ -12,102 +12,105 @@ client = discord.Client()
 discord.opus.load_opus
 roll_command = "!croll"
 
-FirstConnect=True
-LastPlayingIndex=-1
+FirstConnect = True
+LastPlayingIndex = -1
 PlayingQuotes = {
-        1: "with dice",
-        2: "second item"
-    }
+    1: "with dice",
+    2: "second item"
+}
 
-COL_CRIT_SUCCESS=0xFFFFFF
-COL_EXTR_SUCCESS=0xf1c40f
-COL_HARD_SUCCESS=0x2ecc71
-COL_NORM_SUCCESS=0x2e71cc
-COL_NORM_FAILURE=0xe74c3c
-COL_CRIT_FAILURE=0x992d22
+COL_CRIT_SUCCESS = 0xFFFFFF
+COL_EXTR_SUCCESS = 0xf1c40f
+COL_HARD_SUCCESS = 0x2ecc71
+COL_NORM_SUCCESS = 0x2e71cc
+COL_NORM_FAILURE = 0xe74c3c
+COL_CRIT_FAILURE = 0x992d22
+
 
 class DiceResult:
     def __init__(self):
-        self.title=""
-        self.desc=""
-        self.colour=COL_NORM_SUCCESS
-        self.image=""
+        self.title = ""
+        self.desc = ""
+        self.colour = COL_NORM_SUCCESS
+        self.image = ""
+
 
 def RollDie(min=1, max=10):
-    result = randint(min,max)
+    result = randint(min, max)
     return result
 
 
-
 def ResolveDice(BonusDie, PenaltyDie, Threshold):
-  TenResultPool = []
-  TenResultPool.append(RollDie(0, 9))
+    TenResultPool = []
+    TenResultPool.append(RollDie(0, 9))
 
-  TenResult = min(TenResultPool)
-  OneResult = RollDie()
+    TenResult = min(TenResultPool)
+    OneResult = RollDie()
 
-  if BonusDie > 0 and PenaltyDie > 0:
-      return "Can't chain bonus and penalty dice"
+    if BonusDie > 0 and PenaltyDie > 0:
+        return "Can't chain bonus and penalty dice"
 
-  for i in range(BonusDie):
-      TenResultPool.append(RollDie(0, 9))
-      TenResult = min(TenResultPool)
-  
-  for i in range(PenaltyDie):
-      TenResultPool.append(RollDie(0, 9))
-      TenResult = max(TenResultPool)
+    for i in range(BonusDie):
+        TenResultPool.append(RollDie(0, 9))
+        TenResult = min(TenResultPool)
 
-  CombinedResult = (TenResult*10) + OneResult
-  
-  if BonusDie > 0 or PenaltyDie > 0: 
-    desc = '(' + '|'.join([str(i * 10) for i in TenResultPool]) + ') ' + str(TenResult * 10) + ' + ' + str(OneResult) + ' = ' + str(CombinedResult)
-  else: 
-    desc = str(TenResult * 10) + ' + ' + str(OneResult) + ' = ' + str(CombinedResult)
+    for i in range(PenaltyDie):
+        TenResultPool.append(RollDie(0, 9))
+        TenResult = max(TenResultPool)
 
-  if Threshold:
-    ret = DiceResult()
-    if CombinedResult == 1:
-      ret.title = "Critical Success!"
-      ret.colour = COL_CRIT_SUCCESS
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042867138134066/nat20Smaller.png"
-    elif CombinedResult <= 5 and Threshold > 50:
-      ret.title = "Critical Success!"
-      ret.colour = COL_CRIT_SUCCESS
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042867138134066/nat20Smaller.png"
-    elif CombinedResult == 100:
-      ret.title = "Critical Failure!"
-      ret.colour = COL_CRIT_FAILURE
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042861928022047/nat1smaller.png"
-    elif CombinedResult > 95 and Threshold <= 50:
-      ret.title = "Critical Failure!"
-      ret.colour = COL_CRIT_FAILURE
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042861928022047/nat1smaller.png"
-    elif CombinedResult <= Threshold/5:
-      ret.title = "Extreme Success!"
-      ret.colour = COL_EXTR_SUCCESS
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779041383959756860/yaythulhuSmaller.png"
-    elif CombinedResult <= Threshold/2:
-      ret.title = "Hard Success!"
-      ret.colour = COL_HARD_SUCCESS
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779041377534869533/babythulhuSmaller.png"
-    elif CombinedResult <= Threshold:
-      ret.title = "Success!"
-      ret.colour = COL_NORM_SUCCESS
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779043134020780062/cutethulhuSmaller.png"
+    CombinedResult = (TenResult * 10) + OneResult
+
+    if BonusDie > 0 or PenaltyDie > 0:
+        desc = '(' + '|'.join([str(i * 10) for i in TenResultPool]) + ') ' + str(TenResult * 10) + ' + ' + str(
+            OneResult) + ' = ' + str(CombinedResult)
     else:
-      ret.title = "Failure"
-      ret.colour = COL_NORM_FAILURE
-      ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779043270948421662/deniedSmaller.png"
-	
-	#ret.title = ret.title + str(CombinedResult)
-    ret.desc = desc
-    return ret
-  else:
-    ret = desc
-    return ret	
+        desc = str(TenResult * 10) + ' + ' + str(OneResult) + ' = ' + str(CombinedResult)
+
+    if Threshold:
+        ret = DiceResult()
+        if CombinedResult == 1:
+            ret.title = "Critical Success!"
+            ret.colour = COL_CRIT_SUCCESS
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042867138134066/nat20Smaller.png"
+        elif CombinedResult <= 5 and Threshold > 50:
+            ret.title = "Critical Success!"
+            ret.colour = COL_CRIT_SUCCESS
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042867138134066/nat20Smaller.png"
+        elif CombinedResult == 100:
+            ret.title = "Critical Failure!"
+            ret.colour = COL_CRIT_FAILURE
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042861928022047/nat1smaller.png"
+        elif CombinedResult > 95 and Threshold <= 50:
+            ret.title = "Critical Failure!"
+            ret.colour = COL_CRIT_FAILURE
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779042861928022047/nat1smaller.png"
+        elif CombinedResult <= Threshold / 5:
+            ret.title = "Extreme Success!"
+            ret.colour = COL_EXTR_SUCCESS
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779041383959756860/yaythulhuSmaller.png"
+        elif CombinedResult <= Threshold / 2:
+            ret.title = "Hard Success!"
+            ret.colour = COL_HARD_SUCCESS
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779041377534869533/babythulhuSmaller.png"
+        elif CombinedResult <= Threshold:
+            ret.title = "Success!"
+            ret.colour = COL_NORM_SUCCESS
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779043134020780062/cutethulhuSmaller.png"
+        else:
+            ret.title = "Failure"
+            ret.colour = COL_NORM_FAILURE
+            ret.image = "https://cdn.discordapp.com/attachments/778900221199253505/779043270948421662/deniedSmaller.png"
+
+        # ret.title = ret.title + str(CombinedResult)
+        ret.desc = desc
+        return ret
+    else:
+        ret = desc
+        return ret
+
 
 def parseRoll(diceString):
-    fail="""
+    fail = """
 Unable to parse dice command. Usage:
 ```
 !croll [[die type][number=1]]...[[threshold][score]]
@@ -131,36 +134,54 @@ Examples:
     Failure: 0/50/70 + 4 = 74
 ```
 """
-    dice=[x for x in re.split('([bpt](?:\d+)?)',diceString) if x]
-    
+
+    # check for the case where ppl forget to write t
+    # handle case where there is no bpt
+    if diceString.isnumeric():
+        return ResolveDice(0, 0, int(diceString))
+
+    dice = [x for x in re.split('([bpt](?:\d+)?)', diceString) if x]
+
     if len(dice) > 1 and 'b' in diceString and 'p' in diceString:
         return "Can't chain bonus and penalty dice"
-    
-    BonusDie=0
-    PenaltyDie=0
-    Threshold=False
+
+    BonusDie = 0
+    PenaltyDie = 0
+    Threshold = False
 
     for die in dice:
-        default_num = False
-        s=re.search('([bpt])(\d+)?', die)
-        if not s:
-            default_num = True
-            die=die+"1"
-        s=re.search('([bpt])(\d+)?', die)
-        if not s:
 
+        default_num = False
+        # match string
+        die_string = re.search('([bpt])(\d+)?', die)
+        # for b and p, add dice number 1
+        if not die_string:
+            default_num = True
+            die = die + "1"
+
+        # try to match it again
+        die_string = re.search('([bpt])(\d+)?', die)
+        # still didn't work
+        if not die_string:
             return fail
-        g=s.groups()
-        if len(g) != 2:
+
+        # get the matched groups
+        die_groups = die_string.groups()
+        # too many ?
+        if len(die_groups) != 2:
             return fail
+
+        # parse number of dice
         try:
-            num=int(g[1])
+            num = int(die_groups[1])
         except:
             default_num = True
-            num=1
+            num = 1
 
-        dieCode=g[0]
-        
+        # get die code (b p t)
+        dieCode = die_groups[0]
+
+        # too long of a dice code?
         if len(dieCode) > 1:
             return fail
 
@@ -170,22 +191,24 @@ Examples:
         if dieCode == 'p':
             PenaltyDie = num
 
-        if  dieCode == 't':
+        if dieCode == 't':
             if default_num:
-              return "Threshold requires a value!"
+                return "Threshold requires a value!"
             else:
-              Threshold = num
-        
+                Threshold = num
+
     return ResolveDice(BonusDie, PenaltyDie, Threshold)
+
 
 async def cyclePlaying():
     global LastPlayingIndex
-    playing=PlayingQuotes[randint(1,len(PlayingQuotes))]
+    playing = PlayingQuotes[randint(1, len(PlayingQuotes))]
     while playing == LastPlayingIndex:
-        playing=PlayingQuotes[randint(1,len(PlayingQuotes))]
-    LastPlayingIndex=playing
-    #await client.change_presence(game=discord.Game(name=playing))
-    await asyncio.sleep(randint(60,600))
+        playing = PlayingQuotes[randint(1, len(PlayingQuotes))]
+    LastPlayingIndex = playing
+    # await client.change_presence(game=discord.Game(name=playing))
+    await asyncio.sleep(randint(60, 600))
+
 
 @client.event
 async def on_ready():
@@ -193,26 +216,27 @@ async def on_ready():
     print("Dorian connected")
     if FirstConnect:
         FirstConnect = False
-        #while True:
-            #await asyncio.ensure_future(cyclePlaying())
-        
+        # while True:
+        # await asyncio.ensure_future(cyclePlaying())
+
+
 @client.event
 async def on_message(message):
-
     if message.author == client.user:
         return
 
     if message.content.startswith(roll_command):
-        result = parseRoll(message.content[len(roll_command)+1:])
+        result = parseRoll(message.content[len(roll_command) + 1:])
         if isinstance(result, str):
             await message.channel.send(result)
         else:
             em = discord.Embed(title=result.title, description=result.desc, colour=result.colour)
             em.set_footer(text=result.desc)
             em.set_image(url=result.image)
-            em.description=None
+            em.description = None
             await message.channel.send(message.author.mention, embed=em)
-            #await message.channel.send()
-    
-token=environ['DORIAN_TOKEN']
+            # await message.channel.send()
+
+
+token = environ['DORIAN_TOKEN']
 client.run(token)
